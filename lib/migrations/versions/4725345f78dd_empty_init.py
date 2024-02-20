cd0b69cc6f16d47f00b96ec6dfcbd589efb65baf
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2024-02-20 16:57:06 +1100

    complete

---
+++ lib/migrations/versions/4725345f78dd_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 4725345f78dd
+Revises: 
+Create Date: 2024-02-20 16:44:05.070832
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4725345f78dd'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
